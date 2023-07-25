b4ca0a239cedbefcc03d155f3ed9e0944f55d03c
JohnnyBoi03 <johnnyblogs01@gmail.com>
2023-07-25 13:28:46 +0100

    floating button

--- lib/screens/detail/detail.dart
+++ lib/screens/detail/detail.dart
@@ -1,7 +1,10 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/constants/colors.dart';
 import 'package:food_delivery_app/screens/detail/widgets/food_detail.dart';
 import 'package:food_delivery_app/screens/detail/widgets/food_img.dart';
+//import 'package:food_delivery_app/screens/detail/widgets/food_quantity.dart';
 import 'package:food_delivery_app/widgets/custom_app_bar.dart';
 
 import '../../models/food.dart';
@@ -25,6 +28,40 @@ class DetailPage extends StatelessWidget {
             FoodImg(food),
             FoodDetail(food),
           ],
+        ),
+      ),
+      floatingActionButton: Container(
+        width: 100,
+        height: 56,
+        child: RawMaterialButton(
+          fillColor: kPrimaryColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(50),
+          ),
+          elevation: 2,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(
+                Icons.shopping_bag_outlined,
+                color: Colors.black,
+                size: 30,
+              ),
+              Container(
+                padding: EdgeInsets.all(
+                  15,
+                ),
+                decoration:
+                    BoxDecoration(color: Colors.white, shape: BoxShape.circle),
+                child: Text(food.quantity.toString(),
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black)),
+              )
+            ],
+          ),
+          onPressed: () {},
         ),
       ),
     );
